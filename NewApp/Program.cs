629db5aea22b94fc61506362131bddebb2fd865e
hoanghavctd <hoanghavctd@gmail.com>
2023-08-07 16:11:47 +0700

    Update_Code

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 9;
+int b = 8;
+int c = 7;
+Console.WriteLine("So a: " +a);
+Console.WriteLine("So b: " +b);
+Console.WriteLine("So c: " +c);
+Console.WriteLine("Ket qua a + b la: {0} + {1} = {2}", a, b, a+b);
+Console.WriteLine("Ket qua a * c la: {0} * {1} = {2}", a, c, a*c);
