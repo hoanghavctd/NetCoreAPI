68fbcbe0ffb9bfac3ff68da7cd4e409b5e004c70
hoanghavctd <hoanghavctd@gmail.com>
2023-08-08 14:47:15 +0700

    Update_Code

--- NewApp/Program.cs
+++ NewApp/Program.cs
@@ -1,8 +1,4 @@
-﻿int a = 9;
-int b = 8;
-int c = 7;
-Console.WriteLine("So a: " +a);
-Console.WriteLine("So b: " +b);
-Console.WriteLine("So c: " +c);
-Console.WriteLine("Ket qua a + b la: {0} + {1} = {2}", a, b, a+b);
-Console.WriteLine("Ket qua a * c la: {0} * {1} = {2}", a, c, a*c);
+﻿using NewApp.Models;
+Person ps = new Person();
+ps.EnterData();
+ps.Display();
